7b6ec7364dd709b6527f51810edfd1debad6129a
reslario <44400244+reslario@users.noreply.github.com>
2020-02-09 17:54:58 +0100

    Slightly adjust db models' data types / visibility

--- src/models/db/models.rs
+++ src/models/db/models.rs
@@ -4,39 +4,42 @@ use {
 };
 
 const NUM_ANSWERS: usize = 4;
+const NUM_INCORRECT: usize = NUM_ANSWERS - 1;
 
 #[derive(Identifiable, Queryable, Associations, PartialEq, Debug)]
 #[belongs_to(Category)]
 #[table_name = "questions"]
 pub struct Question {
-    id: u32,
-    category_id: u32,
-    string: String,
-    correct: String,
-    incorrect: [String; NUM_ANSWERS - 1]
+    pub(super) id: i32,
+    pub(super) category_id: i32,
+    pub string: String,
+    pub correct: String,
+    #[diesel(deserialize_as = "Incorrect")]
+    pub incorrect: [String; NUM_INCORRECT]
 }
 
 #[derive(Identifiable, Queryable, Associations, PartialEq, Debug)]
 #[belongs_to(Question)]
 #[table_name = "question_stats"]
 pub struct QuestionStats {
-    id: u32,
-    question_id: u32,
-    num_correct: u32,
-    num_incorrect: u32
+    pub(super) id: i32,
+    pub(super) question_id: i32,
+    pub num_correct: i32,
+    pub num_incorrect: i32
 }
 
 #[derive(Identifiable, Queryable, PartialEq, Debug)]
 #[table_name = "categories"]
 pub struct Category {
-    id: u32,
-    name: String
+    pub(super) id: i32,
+    pub name: String
 }
 
 #[derive(Identifiable, Queryable, PartialEq, Debug)]
 #[table_name = "scores"]
 pub struct Score {
-    id: u32,
-    user: String,
-    points: u32
+    pub(super) id: i32,
+    pub name: String,
+    pub points: i32
+}
 }
